ee5d26b9e3068dc1d79dcd2adb6a7ceaa53a7ba0
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-19 20:45:34 -0700

    worked on football.php

--- web/Football.php
+++ web/Football.php
@@ -85,7 +85,7 @@
 		$query2 = "SELECT Analysis.Team_id, Analysis.Week_id, Score.teamScore, Score.oppScore, Score.iswin, Spread.proj_spread, Score.realSpread, Analysis.spreadDifference FROM ((Analysis INNER JOIN Spread ON Analysis.spread_id = Spread.id) INNER JOIN Score ON Analysis.score_id = Score.id) WHERE Analysis.Team_id = " . $teamID . ";";
 		foreach ($db->query($query2) as $row) {	
 			echo $teamID;
-			echo "<tr><td>" $row['week_id'] .  "</td><td>"; 
+			echo "<tr><td>" . $row['week_id'] .  "</td><td>"; 
 			echo $row['teamscore'] . "</td><td>"; 
 			echo $row['oppscore'] . "</td><td>"; 
 			echo $row['iswin'] . "</td><td>"; 
