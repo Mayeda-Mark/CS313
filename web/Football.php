54dbb7eb06914a9e97006cd9522760ca25d65084
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-27 11:07:03 -0700

    worked on football.php

--- web/Football.php
+++ web/Football.php
@@ -1,8 +1,13 @@
 <?php  
+/* TODO:
+	-Make select option filler function
+	-Clean up getData
+	-Dynamic borders?
+	-Fix ordering in getData (by week)
+*/
 require("dbConnect.php");
 require("getData.php");
 require("addData.php");
-//require("editData.php");
 $db = get_db();
 ?>
 <!DOCTYPE html>
@@ -58,128 +63,6 @@ $db = get_db();
 		</form>
 	</div>
 		<br>
-	<div id="results1" style="text-align: center">
-	<?php 
-
-	getData();
-	insertData();
-	/*
-		//Set Variables
-		$iswin = false;
-		$teamName = $_POST['Team1'];
-		$teamName2 = $_POST['Team2'];
-		$weekNumber = $_POST['weekNumber'];
-		$score = $_POST['score'];
-		$opponentScore = $_POST['opponentScore'];
-		$projectedSpread = $_POST['projectedSpread'];
-		$actualSpread = $_POST['actualSpread'];
-		$totalWins = 0;
-		$totalDifference = 0;
-		$weeks = 0;
-		$totalLosses = 0;
-		$totalDraws = 0;
-
-			//Table goes here if error
-
-
-
-
-		if ($teamName2 != "" && $weekNumber != "" && $score != "" && $opponentScore != "" && $projectedSpread != "" && $actualSpread != "") {
-			//Get team id
-		$query3 = "SELECT id FROM Team WHERE Name = '" . $teamName2 . "';";
-		foreach ($db->query($query3) as $team) {
-
-			$teamID = $team['id'];
-		}
-		
-		if ($score > $opponentScore) {
-			$iswin = true;
-		}
-		
-		$query4 = "INSERT INTO Score (Team_id, Week_id, TeamScore, OppScore, realSpread, isWin)
-		VALUES (" . $teamID . " ," . $weekNumber . " ," . $score . " ," . $opponentScore . " ," . $actualSpread . " ," . $iswin . ");"; 
-
-		$query5 = "INSERT INTO Spread (Team_id, Week_id, proj_spread)
-		VALUES (" . $teamID . " ," . $weekNumber . " ," . $projectedSpread . ");";
-
-		$query6 = "INSERT INTO Analysis (Team_id, Week_id, spread_id, score_id, spreaddifference)
-		VALUES"
-
-		$db->query(query4);
-		$db->query(query5);
-
-	}
-
-		else {*/
-
-		//Set up table
-		/*if ($teamName != "") {
-		echo "<h1 style=\"text-align: left\">" . $teamName . "</h1><table border=\"1\"><tr>";
-		echo "<th>Week</th>";
-		echo "<th>Score</th>";
-		echo "<th>Opponent Score</th>";
-		echo "<th>W/L</th>";
-		echo "<th>Projected Spread</th>";
-		echo "<th>Actual Spread</th>";
-		echo "<th>Spread Difference</th></tr>";
-
-		//Get team id
-		$query1 = "SELECT id FROM Team WHERE Name = '" . $teamName . "';";
-		foreach ($db->query($query1) as $team) {
-			$teamID = $team['id'];
-		}
-
-		//Get table
-		$query2 = "SELECT Analysis.Team_id, Analysis.Week_id, Score.teamScore, Score.oppScore, Score.iswin, Spread.proj_spread, Score.realSpread, Analysis.spreadDifference FROM ((Analysis INNER JOIN Spread ON Analysis.spread_id = Spread.id) INNER JOIN Score ON Analysis.score_id = Score.id) WHERE Analysis.Team_id = " . $teamID . ";";
-
-		//Fill table
-		foreach ($db->query($query2) as $row) {	
-			//Week
-			echo "<tr><td><style= 'text-align:center'>" . $row['week_id'] .  "</td><td>"; 
-			//Team Score
-			echo $row['teamscore'] . "</td><td>"; 
-			//Oponent score
-			echo $row['oppscore'] . "</td>"; 
-			//Win/Loss
-			if ($row['iswin']) {
-				echo "<td>W</td><td>"; 
-				$totalWins++;
-				}
-			elseif ($row['teamscore'] == $row["oppscore"]) {
-				echo "<td>D</td><td>";
-				$totalDraws++;
-			}
-			else {
-				echo "<td style='color:red;'>L</td><td>"; 
-				$totalLosses++;
-			}
-			//Projected Spread
-			echo $row['proj_spread'] . "</td><td>";
-			//Actual Spread
-			echo $row['realspread'] . "</td>";
-			//Spread Difference
-			if ($row['spreaddifference'] < 0) {
-			echo "<td style='color:red;'>" . $row['spreaddifference'] . "</td></tr>";
-			}
-			else {
-			echo "<td>" . $row['spreaddifference'] . "</td></tr>";	
-			}
-			//Inciment Weeks
-			$totalDifference += $row['spreaddifference'];
-			$weeks++;
-	}
-	echo "</table>";
-	echo "<p style=\"text-align:left\">Win/Loss Record: " . $totalWins . "/" . $totalLosses;
-	if ($totalDraws) {
-		echo "/" . $totalDraws;
-	}
-	echo "<br>Average spread difference: " . $totalDifference / $weeks . "</p>";
-}
-}
-*/
-
-	?>
-	</div>
 	<div>
 		<span>Add or Edit Game Information</span>
 	<form id="edit"  method="post" action="Football.php">
@@ -216,7 +99,7 @@ $db = get_db();
 				<option value="Jacksonville Jaguars">Jacksonville Jaguars</option>
 				<option value="Indianapolis Colts">Indianapolis Colts</option>
 				<option value="Tennesse Titans">Tennesse Titans</option>
-				<option value="Tampa Bay Buccaneers">Tamps Bay Buccaneers</option>
+				<option value="Tampa Bay Buccaneers">Tampa Bay Buccaneers</option>
 			</select>
 			Week Number: <select name="weekNumber">
 				<option value="" selected>-Select-</option>
@@ -245,6 +128,14 @@ $db = get_db();
 			<br>
 			<button type="submit" form="edit">Submit</button>
 		</form>
+	</div>
+	<div id="results" style="text-align: center">
+	<?php 
+
+	getData();
+	insertData();
+	
+	?>
 	</div>
 </body>
 </html>
