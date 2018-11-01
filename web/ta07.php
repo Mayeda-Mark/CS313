76b4636e52b2aff226111bf6cb9954622d84e39d
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-31 20:08:03 -0700

    did stuff

---
+++ web/ta07.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+<body>
+	<form name="sign up" method="post">
+		Name: <input type="text" name="name">
+		Password: <input type="text" name="password">
+		<input type="submit" name="sign up" value="Submit">
+	</form><!--
+	<?php 
+	require("dbConnect.php");
+	$db = get_db();
+	$name = $_POST['name'];
+	$password = $_POST['password'];
+	$newUrl = "signin.php";
+
+	if(isset($name) && isset($password)) {
+		$query = "INSERT INTO web_users (user_name, password)
+		VALUES(:name, :password);";
+
+		$statement = $db->prepare($query);
+
+		$statement->bindValue(':name', $name);
+		$statement->bindValue(':password', $password);
+
+		$statement->execute();
+		header('location'.$newUrl);
+	}
+
+
+
+	?>-->
+</body>
+</html>
